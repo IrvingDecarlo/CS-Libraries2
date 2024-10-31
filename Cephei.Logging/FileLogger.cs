96ab7d2b5a4985b8c9a4a668d1854768fc2b76ec
Irving Decarlo <ivbrdc@gmail.com>
2024-10-31 18:21:01 -0300

    Cephei.Logging updated to 1.1

--- Cephei.Logging/FileLogger.cs
+++ Cephei.Logging/FileLogger.cs
@@ -1,55 +1,35 @@
-﻿using System;
-using System.IO;
-
-namespace Cephei.Logging
+﻿namespace Cephei.Logging
 {
   /// <summary>
   /// The FileLogger implements the Logger class by outputting data to an appended file.
   /// </summary>
-  public abstract class FileLogger : Logger, IDisposable
+  public class FileLogger : FileLoggerAbstract
   {
     /// <summary>
     /// Creates a FileLogger object, creating a new (or appending to) a file.
     /// </summary>
     /// <param name="path">Path to the file.</param>
+    /// <param name="level">The logger's initial log level.</param>
     /// <remarks>Does not create the directories that lead to the file.</remarks>
-    public FileLogger(string path) => Writer = new StreamWriter(path, true);
-
-    /// <summary>
-    /// Disposes of the FileLogger.
-    /// </summary>
-    ~FileLogger() => Dispose();
+    public FileLogger(string path, LogLevel level = LogLevel.Information) : base(path) 
+      => LogLevel = level;
 
     #region override
 
     /// <summary>
-    /// Gets the writer used by the logger.
+    /// Gets the logger's log level.
     /// </summary>
-    /// <returns>The writer used by the logger.</returns>
-    protected override TextWriter? GetLogWriter() => Writer;
-
-    /// <summary>
-    /// Disposes of the FileLogger, disposing its underlying stream.
-    /// </summary>
-    public void Dispose()
-    {
-      OnDispose();
-      Writer?.Dispose();
-    }
-
-    /// <summary>
-    /// Additional actions to take when the logger is disposed.
-    /// </summary>
-    protected abstract void OnDispose();
+    /// <returns>The logger's log level.</returns>
+    protected override LogLevel GetLogLevel() => LogLevel;
 
     #endregion
 
-    #region protected
+    #region public
 
     /// <summary>
-    /// Reference to the writer used by the logger.
+    /// Defines the logger's log level.
     /// </summary>
-    protected readonly TextWriter Writer;
+    public LogLevel LogLevel;
 
     #endregion
   }
---
+++ Cephei.Logging/FileLoggerAbstract.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace Cephei.Logging
+{
+  /// <summary>
+  /// The FileLogger implements the Logger class by outputting data to an appended file.
+  /// </summary>
+  public abstract class FileLoggerAbstract : Logger
+  {
+    /// <summary>
+    /// Creates a FileLogger object, creating a new (or appending to) a file.
+    /// </summary>
+    /// <param name="path">Path to the file.</param>
+    /// <remarks>Does not create the directories that lead to the file.</remarks>
+    public FileLoggerAbstract(string path) => Writer = new StreamWriter(path, true);
+
+    #region override
+
+    /// <summary>
+    /// Gets the writer used by the logger.
+    /// </summary>
+    /// <returns>The writer used by the logger.</returns>
+    protected override TextWriter? GetLogWriter() => Writer;
+
+    /// <summary>
+    /// Disposes of the FileLogger, disposing its underlying stream.
+    /// </summary>
+    public override void Dispose() => Writer?.Dispose();
+
+    #endregion
+
+    #region protected
+
+    /// <summary>
+    /// Reference to the writer used by the logger.
+    /// </summary>
+    protected readonly TextWriter Writer;
+
+    #endregion
+  }
+}
