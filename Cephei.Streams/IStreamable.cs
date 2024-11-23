6c2f6d2b5b5ed1aaf7efc17517ebba57dfcbf165
IrvingDecarlo <ivbrdc@gmail.com>
2024-11-23 13:49:09 -0300

    Cephei.Streams updated to 1.2.3.

---
+++ Cephei.Streams/IStreamable.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Cephei.Streams
+{
+  /// <summary>
+  /// The IStreamable interface denotes an object that can yield a Stream.
+  /// </summary>
+  public interface IStreamable
+  {
+    /// <summary>
+    /// Gets the object's stream.
+    /// </summary>
+    /// <returns>The object's stream.</returns>
+    Stream GetStream();
+  }
+}
